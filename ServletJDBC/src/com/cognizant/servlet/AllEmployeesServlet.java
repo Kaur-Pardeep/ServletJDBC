e90544d3970ebe5a50814dabb395da42ca342f06
Kaur-Pardeep <pardeepkaur245@gmail.com>
2019-02-18 17:56:21 +0530

    Add files via upload

---
+++ ServletJDBC/src/com/cognizant/servlet/AllEmployeesServlet.java
@@ -0,0 +1,49 @@
+package com.cognizant.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AllEmployeesServlet
+ */
+@WebServlet(
+		urlPatterns = { "/AllEmployeesServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "driver", value = "oracle.jdbc.OracleDriver"), 
+				@WebInitParam(name = "url", value = "jdbc:oracle:thin:@localhost:1522:xe"), 
+				@WebInitParam(name = "Username", value = "system"), 
+				@WebInitParam(name = "Password", value = "cognizant")
+		})
+public class AllEmployeesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AllEmployeesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
